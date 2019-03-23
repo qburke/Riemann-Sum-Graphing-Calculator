87b62edb833856625c8e4c205d704c812e14a3c1
qburke <burkequin@gmail.com>
2019-03-23 12:57:15 -0400

    1/x works for positive values

--- RS-GCalc.py
+++ RS-GCalc.py
@@ -1,59 +1,64 @@
 # Quin Burke 2019
-from tkinter import * as tk
+import Tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
 
 def func_parser(f):
-  return lambda x : x
+    return lambda x : 1/x
 
 def graph_sum():
-  b = int(upr_bnd_fld.get())
-  a = int(lwr_bnd_fld.get())
-  n = int(sub_int_fld.get())
-  f = func_parser(func_fld.get())
+    b = int(upr_bnd_fld.get())
+    a = int(lwr_bnd_fld.get())
+    n = int(sub_int_fld.get())
+    f = func_parser(func_fld.get())
   
-  x_dscr = np.linspace(a,b,n+1)
-  y_dscr = f(x)
+    x_dscr = np.linspace(a,b,n+1)
+    y_dscr = f(x_dscr)
   
-  x_cont = np.linspace(a,b,100)
-  y_cont = f(X)
+    x_cont = np.linspace(a-1,b+1,100)
+    y_cont = f(x_cont)
   
-  plt.figure(figsize=(15,5))
-  plt.plot(x_cont,y_cont,'b')
-  if hg_mtd == 0: # Left Riemann
-    x = x_dscr[:-1]
-    y = y_dscr[:-1]
-  elif hg_mtd == 1: # Right Riemann
-    x = x_dscr[1:]
-    y = y_dscr[1:]
-  elif hg_mtd == 2: # Midpoint
-    x = (x_dscr[:-1] + x_dscr[1:])/2
-    y = f(x)
-  plt.plot(x,y,'b.',markersize=8)
-  plt.bar(x,y,width=(b-a)/n,alpha=0.2,align='edge',edgecolor='b')
-  plt.title('Left Riemann Sum with {} Sub-Intervals'.format(n))
-  plt.show() # embed in window later
+    plt.figure(figsize=(15,5))
+    plt.plot(x_cont,y_cont,'b')
+    hg = hg_mtd.get()
+    dx = (b-a)/n
+    alignment = 'edge'
+    if hg == 0: # Left Riemann
+        x_dscr = x_dscr[:-1]
+        y_dscr = y_dscr[:-1]
+    elif hg == 1: # Right Riemann
+        x_dscr = x_dscr[1:]
+        y_dscr = y_dscr[1:]
+        dx = -(b-a)/n
+    elif hg == 2: # Midpoint
+        x_dscr = (x_dscr[:-1] + x_dscr[1:])/2
+        y_dscr = f(x_dscr)
+        alignment='center'
+    plt.plot(x_dscr,y_dscr,'b.',markersize=8)
+    plt.bar(x_dscr,y_dscr,width=dx,alpha=0.2,align=alignment,edgecolor='b')
+    plt.title('Left Riemann Sum with {} Sub-Intervals'.format(n))
+    plt.show() # embed in window later
   
 root = tk.Tk()
 
 # Function
-Label(master, text="Function:").pack()
-func_fld = Entry(master)
+tk.Label(root, text="Function:").pack()
+func_fld = tk.Entry(root)
 func_fld.pack()
 
-# Upper Bound
-Label(master, text="Upper Bound:").pack()
-upr_bnd_fld = Entry(master)
-upr_bnd_fld.pack()
-
 # Lower Bound
-Label(master, text="Lower Bound:").pack()
-lwr_bnd_fld = Entry(master)
+tk.Label(root, text="Lower Bound:").pack()
+lwr_bnd_fld = tk.Entry(root)
 lwr_bnd_fld.pack()
 
+# Upper Bound
+tk.Label(root, text="Upper Bound:").pack()
+upr_bnd_fld = tk.Entry(root)
+upr_bnd_fld.pack()
+
 # Sub-Intervals
-Label(master, text="Sub-Intervals (n):").pack()
-sub_int_fld = Entry(master)
+tk.Label(root, text="Sub-Intervals (n):").pack()
+sub_int_fld = tk.Entry(root)
 sub_int_fld.pack()
 
 # Height Generation Method
@@ -64,27 +69,23 @@ hg_mtds = ["Left", "Right", "Midpoint"]
 
 tk.Label(root,
   text="Select a height generation method:",
-  justfiy=tk.LEFT,
   padx=20).pack()
 
 for i, mtd in enumerate(hg_mtds):
-  tk.Radiobutton(root,
-    text=mtd,
-    padx=20
-    variable=hg_mtd,
-    value=i).pack(anchor=tk.W)
+    tk.Radiobutton(root,
+                   text=mtd,
+                   padx=20,
+                   variable=hg_mtd,
+                   value=i).pack(anchor=tk.W)
 
 # Graph It! Button
-Button(master, text="Graph It!", command=graph_sum).grid(row=3, column=1, sticky=W, pady=4)
+tk.Button(root, text="Graph It!", command=graph_sum).pack()
 
 # Error field
-Label(master, text="Please enter the required information.").pack()
+tk.Label(root, text="Please enter the required information.").pack()
 
 # function in math type?
 
 # graph of function with rectangles
 
 root.mainloop()
-
-
-# Note: pyinstaller to make exe
